6b96f3dd829f2bd6f588c7ff4059e2f19e92b8eb
Ari P <ariteknologi@gmail.com>
2024-08-07 18:17:22 +0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: process.env.NEXT_PUBLIC_BASE_URL,
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_URL,
+  basePath: process.env.NEXT_PUBLIC_BASE_URL,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_URL,
   output: 'standalone',
   images: {
     unoptimized: true,
