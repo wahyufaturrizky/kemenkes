fb2d3493844cf56a48c5f565248b4c3f00179f66
wahyu.rizki <wahyu.rizki@accenture.com>
2024-10-10 09:44:56 +0700

    FEAT: component graph analisis risiko

--- src/view/dashboard/analisis-faktor-risiko/index.tsx
+++ src/view/dashboard/analisis-faktor-risiko/index.tsx
@@ -1,32 +1,25 @@
 "use client";
-import { DownloadButton, GraphEcharts, Select } from "@/components";
+import { DownloadButton, GraphEcharts } from "@/components";
 import ButtonIcon from "@/components/button-icon";
-import CardPemeriksaan from "@/components/cardPemeriksaan";
+import GraphItem from "@/components/graph-item";
 import Header from "@/components/header";
 import Progress from "@/components/progress";
 import SectionHeader from "@/components/sectionHeader";
 import { formatNumber } from "@/helpers";
 import {
-  useGetAnemiaScreeningQuery,
-  useGetBloodPresureQuery,
-  useGetBodyMassIndexAgeQuery,
-  useGetCigaretteSmokingQuery,
-  useGetFitnessQuery,
-  useGetHealthQuery,
-  useGetHearingQuery,
-  useGetMentalHealthQuery,
-  useGetNapzaScreeningQuery,
-  useGetSmokingQuery,
-  useGetVisionQuery,
-} from "@/lib/services/monitoring-faktor-risiko";
+  useGetGraphImmunizationScopeQuery,
+  useGetHighestScopeImmunizationQuery,
+  useGetLowestScopeImmunizationQuery,
+  useGetTotalBayiImmunizationScopeQuery,
+} from "@/lib/services/baby-immunization";
 import { ancGtaphOptions5, dataMonth } from "@/utils/constants";
+import { graphOptions5 } from "@/view/graphOptions";
 import FilterMonitoringFaktorRisiko from "@/view/home/components/FilterMonitoringFaktorRisiko";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { IoMdArrowForward, IoMdInformationCircleOutline } from "react-icons/io";
+import { graphOptions1 } from "../analisis-faktor-risiko/graphOptions";
 import styles from "../anc/anc.module.css";
 import TableMonitoringFaktorRisiko from "./tableMonitoringFaktorRisiko";
-import GraphItem from "@/components/graph-item";
-import { graphOptions5 } from "@/view/graphOptions";
 
 export default function AnalisisFaktorRisiko() {
   const filterState = useState({
@@ -49,6 +42,8 @@ export default function AnalisisFaktorRisiko() {
     tipe_vaksin4: "bias",
     tipe_vaksin5: "bias",
     tren_type: "kumulatif",
+    tipe_vaksin1: "IMUNISASI DASAR LENGKAP",
+    wilayah1: "province",
   });
   const [filter] = filterState;
 
@@ -64,38 +59,7 @@ export default function AnalisisFaktorRisiko() {
       (!filter.bulan && (!filter.provinsi || !filter.kabkota || !filter.kecamatan)),
   };
 
-  const { data: BodyMassIndexAge, isFetching: isLoadingBodyMassIndexAge } =
-    useGetBodyMassIndexAgeQuery(dateQuery, optionQuery);
-  const { data: BloodPresure, isFetching: isLoadingBloodPresure } = useGetBloodPresureQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: Vision, isFetching: isLoadingVision } = useGetVisionQuery(dateQuery, optionQuery);
-  const { data: Hearing, isFetching: isLoadingHearing } = useGetHearingQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: MentalHealth, isFetching: isLoadingMentalHealth } = useGetMentalHealthQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: NapzaScreening, isFetching: isLoadingNapzaScreening } = useGetNapzaScreeningQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: Health, isFetching: isLoadingHealth } = useGetHealthQuery(dateQuery, optionQuery);
-  const { data: Fitness, isFetching: isLoadingFitness } = useGetFitnessQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: AnemiaScreening, isFetching: isLoadingAnemiaScreening } =
-    useGetAnemiaScreeningQuery(dateQuery, optionQuery);
-  const { data: Smoking, isFetching: isLoadingSmoking } = useGetSmokingQuery(
-    dateQuery,
-    optionQuery
-  );
-  const { data: CigaretteSmoking, isFetching: isLoadingCigaretteSmoking } =
-    useGetCigaretteSmokingQuery(dateQuery, optionQuery);
+  const [rergionTypeGraph1, setRegionTypeGraph1] = useState("province");
 
   const isBrowser = typeof window !== "undefined";
   const chartOptions: any = {
@@ -129,34 +93,46 @@ export default function AnalisisFaktorRisiko() {
     ],
   };
 
-  const chartOptions2: any = {
-    color: ["#006A65"],
-    tooltip: {
-      trigger: "item",
-      formatter: function (params: any) {
-        return `${params.marker} ${params.name}: ${formatNumber(params.value)}`;
-      },
-    },
-    xAxis: {
-      type: "category",
-      data: dataMonth.map((data: any) => data.label.slice(0, 3)),
-      axisLabel: {
-        interval: 0,
-      },
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3],
-        type: "line",
-      },
-    ],
-  };
-  const dataSD = [30, 40, 50, 60, 80];
-  const dataSMP = [45, 55, 65, 75, 85];
-  const dataSMA = [40, 50, 60, 70, 80];
+  const filterGraph1a = useMemo(
+    () => ({
+      ...dateQuery,
+      region_type: "All",
+      vaccine_type: filter.tipe_vaksin1,
+      faskes_parent: filter.faskes
+        ? filter.faskes
+        : filter.kecamatan
+        ? filter.kecamatan
+        : filter.kabkota
+        ? filter.kabkota
+        : filter.provinsi
+        ? filter.provinsi
+        : "",
+      // faskes_parent: filter.faskes_parent,
+    }),
+    [rergionTypeGraph1, filter, dateQuery]
+  );
+
+  const filterGraph1 = useMemo(
+    () => ({
+      ...dateQuery,
+      region_type: filter.wilayah1,
+      vaccine_type: filter.tipe_vaksin1,
+      faskes_parent: filter.faskes
+        ? filter.faskes
+        : filter.kecamatan
+        ? filter.kecamatan
+        : filter.kabkota
+        ? filter.kabkota
+        : filter.provinsi
+        ? filter.provinsi
+        : "",
+      // faskes_parent: filter.faskes_parent,
+    }),
+    [rergionTypeGraph1, filter, dateQuery]
+  );
+
+  const { data: getGraphImmunizationScope, isLoading: isLoadingGraphImmunizationScope } =
+    useGetGraphImmunizationScopeQuery(filterGraph1, optionQuery);
 
   return (
     <div className={`flex flex-col items-center p-[30px]  ${styles.jakartaFont}`}>
@@ -183,112 +159,179 @@ export default function AnalisisFaktorRisiko() {
       <div className="w-full">
         <FilterMonitoringFaktorRisiko filterState={filterState} />
       </div>
-      <div className="px-4">
-        <SectionHeader title="Capaian Deteksi Dini PTM" subtitle="Faktor Risiko" />
-        <div className="mt-5 grid grid-cols-12 gap-6 w-full">
-          <div className="h-[600px] lg:col-span-5 col-span-12">
-            <div className="grid grid-rows-12 h-full gap-6">
-              <div className="rounded-2xl row-span-5 bg-[#006A65] text-white pl-10 flex flex-col justify-center relative">
-                <div className="absolute top-3 right-3">
-                  <IoMdInformationCircleOutline size={24} color="white" />
-                </div>
-                <p className="text-xl font-normal">Jumlah Peserta Skrining</p>
-                <p className="text-xl font-bold">Aktivtas Fisik</p>
-                <p className="text-4xl font-normal">11,037,458</p>
+      <SectionHeader title="Capaian Deteksi Dini PTM" subtitle="Faktor Risiko" />
+      <div className="mt-5 grid grid-cols-12 gap-6 w-full">
+        <div className="h-[600px] lg:col-span-5 col-span-12">
+          <div className="grid grid-rows-12 h-full gap-6">
+            <div className="rounded-2xl row-span-5 bg-[#006A65] text-white pl-10 flex flex-col justify-center relative">
+              <div className="absolute top-3 right-3">
+                <IoMdInformationCircleOutline size={24} color="white" />
               </div>
-              <div className="rounded-2xl row-span-7 border border-[#D6D6D6] px-4 py-8 flex flex-col justify-between h-full">
-                <p className="font-semibold text-xl">Breakdown per jenis kelamin</p>
-                <div className="grid grid-cols-12 gap-3 max-h-[100px]">
-                  <div className="col-span-4 text-center">
-                    <p className="text-[#3BC6BE] font-semibold mb-7">Laki-laki</p>
-                    <p className="font-semibold text-[#616161]">34,753,536</p>
-                    <p className="font-light text-[#616161]">(41.5%)</p>
-                  </div>
-                  <div className="col-span-4 h-[100px]">
-                    <GraphEcharts graphOptions={chartOptions} />
-                  </div>
-                  <div className="col-span-4 text-center">
-                    <p className="text-[#CF3E53] font-semibold mb-7">Perempuan</p>
-                    <p className="font-semibold text-[#616161]">34,753,536</p>
-                    <p className="font-light text-[#616161]">(41.5%)</p>
-                  </div>
+              <p className="text-xl font-normal">Jumlah Peserta Skrining</p>
+              <p className="text-xl font-bold">Aktivtas Fisik</p>
+              <p className="text-4xl font-normal">11,037,458</p>
+            </div>
+            <div className="rounded-2xl row-span-7 border border-[#D6D6D6] px-4 py-8 flex flex-col justify-between h-full">
+              <p className="font-semibold text-xl">Breakdown per jenis kelamin</p>
+              <div className="grid grid-cols-12 gap-3 max-h-[100px]">
+                <div className="col-span-4 text-center">
+                  <p className="text-[#3BC6BE] font-semibold mb-7">Laki-laki</p>
+                  <p className="font-semibold text-[#616161]">34,753,536</p>
+                  <p className="font-light text-[#616161]">(41.5%)</p>
                 </div>
-                <div className="mt-[10px]">
-                  <Progress
-                    data={[
-                      {
-                        color: "#CF3E53",
-                        label: "Perempuan",
-                        value: 5500,
-                        percentage: 70,
-                      },
-                      {
-                        color: "#3BC6BE",
-                        label: "Laki-laki",
-                        value: 3500,
-                        percentage: 30,
-                      },
-                    ]}
-                  />
+                <div className="col-span-4 h-[100px]">
+                  <GraphEcharts graphOptions={chartOptions} />
+                </div>
+                <div className="col-span-4 text-center">
+                  <p className="text-[#CF3E53] font-semibold mb-7">Perempuan</p>
+                  <p className="font-semibold text-[#616161]">34,753,536</p>
+                  <p className="font-light text-[#616161]">(41.5%)</p>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="h-[600px] lg:col-span-7 col-span-12 rounded-2xl border border-[#D6D6D6] py-8 pl-8 pr-2">
-            <div className="relative h-[full] ">
-              <GraphItem
-                showDownload={false}
-                graphOptions={graphOptions5(
-                  [
+              <div className="mt-[10px]">
+                <Progress
+                  data={[
                     {
-                      name: "Jumlah Bumil Anemia",
-                      data: (ancGtaphOptions5 || [])?.map((r: any) => r?.jumlah) || [],
-                      type: "bar",
-                      label: {
-                        show: false,
-                      },
+                      color: "#CF3E53",
+                      label: "Perempuan",
+                      value: 5500,
+                      percentage: 70,
                     },
                     {
-                      name: "% Bumil Anemia",
-                      data:
-                        (ancGtaphOptions5 || [])?.map((r: any) => r?.persentase_bumil_anemia) || [],
-                      type: "line",
-                      label: {
-                        show: false,
-                        // precision: 1,
-                        // formatter: (params: any) =>
-                        //   `${formatNumber(params.value)}%`,
-                      },
+                      color: "#3BC6BE",
+                      label: "Laki-laki",
+                      value: 3500,
+                      percentage: 30,
                     },
-                    {
-                      name: "Cakupan % Nasional Anemia",
-                      data: (ancGtaphOptions5 || [])?.map((r: any) => r?.persentase_nasional) || [],
-                      type: "line",
-                      label: {
-                        show: false,
-                        // precision: 1,
-                        // formatter: (params: any) =>
-                        //   `${formatNumber(params.value)}%`,
-                      },
-                    },
-                  ],
-                  ancGtaphOptions5?.map((r) => r.region)
-                )}
-              />
-            </div>
-            <div className="w-full flex items-end mt-20">
-              <DownloadButton text="Unduh Excel" />
+                  ]}
+                />
+              </div>
             </div>
           </div>
         </div>
-        <SectionHeader
-          title="Profil Piramida Penduduk Skrining Aktivitas Fisik"
-          subtitle="Profil Piramida Penduduk Skrining Aktivitas Fisik"
-        />
-        <div className="grid grid-cols-12 w-full gap-3 mt-5"></div>
-
-        <TableMonitoringFaktorRisiko titleTable="Tabel Data Agregat" />
+        <div className="h-[600px] lg:col-span-7 col-span-12 rounded-2xl border border-[#D6D6D6] py-8 pl-8 pr-2">
+          <div className="relative h-[full] ">
+            <GraphItem
+              showDownload={false}
+              graphOptions={graphOptions5(
+                [
+                  {
+                    name: "Jumlah Bumil Anemia",
+                    data: (ancGtaphOptions5 || [])?.map((r: any) => r?.jumlah) || [],
+                    type: "bar",
+                    label: {
+                      show: false,
+                    },
+                  },
+                  {
+                    name: "% Bumil Anemia",
+                    data:
+                      (ancGtaphOptions5 || [])?.map((r: any) => r?.persentase_bumil_anemia) || [],
+                    type: "line",
+                    label: {
+                      show: false,
+                      // precision: 1,
+                      // formatter: (params: any) =>
+                      //   `${formatNumber(params.value)}%`,
+                    },
+                  },
+                  {
+                    name: "Cakupan % Nasional Anemia",
+                    data: (ancGtaphOptions5 || [])?.map((r: any) => r?.persentase_nasional) || [],
+                    type: "line",
+                    label: {
+                      show: false,
+                      // precision: 1,
+                      // formatter: (params: any) =>
+                      //   `${formatNumber(params.value)}%`,
+                    },
+                  },
+                ],
+                ancGtaphOptions5?.map((r) => r.region)
+              )}
+            />
+          </div>
+          <div className="w-full flex items-end mt-20">
+            <DownloadButton text="Unduh Excel" />
+          </div>
+        </div>
       </div>
+      <SectionHeader
+        title="Profil Piramida Penduduk Skrining Aktivitas Fisik"
+        subtitle="Profil Piramida Penduduk Skrining Aktivitas Fisik"
+      />
+      <div className={`flex flex-wrap sm:flex-nowrap gap-4 relative`}>
+        <div className="flex-grow">
+          <div className="relative flex justify-center items-center">
+            <div className="w-full h-full overflow-scroll" style={{ minHeight: 550 }} id="graphhhh">
+              <GraphEcharts
+                graphOptions={graphOptions1(
+                  [
+                    {
+                      // @ts-ignore
+                      name: "Persentase",
+                      data:
+                        (getGraphImmunizationScope?.data || [])?.map((r: any) => ({
+                          value: r?.percentage,
+                          itemStyle: {
+                            color: r.faskes_desc === "All" ? "#2D9CED" : undefined,
+                          },
+                        })) || [],
+                      type: "bar",
+                      label: {
+                        show: true,
+                        precision: 1,
+                        position: "right",
+                        // formatter: (params: any) => `${params.value}%`,
+                        formatter: (params: any) => {
+                          const reversedData = (getGraphImmunizationScope?.data || [])
+                            .slice()
+                            .reverse(); // Membuat salinan dan membalik urutan
+                          const totalData = reversedData[params.dataIndex]?.total;
+                          const valueWithComma = params?.value?.toString().replace(".", ",");
+
+                          return filter.wilayah1 === "province" || filter.wilayah1 === "city"
+                            ? `${valueWithComma}% (${formatNumber(totalData)})`
+                            : `(${formatNumber(totalData)})`;
+                        },
+                      },
+                    },
+                    {
+                      name: "Target",
+                      type: "line",
+                      color: "#CD4243",
+                      data:
+                        (getGraphImmunizationScope?.data || [])?.map((r: any) => r?.threshold) ||
+                        [],
+                    },
+                    {
+                      name: "Total Penerima",
+                      type: "line",
+                      color: "#FAC515",
+                      data:
+                        (getGraphImmunizationScope?.data || [])?.map((r: any) => r?.total) || [],
+                      show: false, // Menyembunyikan seri secara default
+                      itemStyle: {
+                        opacity: 0, // Mengatur opacity item menjadi 0 untuk menyembunyikan item
+                      },
+                      lineStyle: {
+                        opacity: 0, // Mengatur opacity garis menjadi 0 untuk menyembunyikan garis
+                      },
+                    },
+                  ],
+                  (getGraphImmunizationScope?.data || [])?.map((r: any) =>
+                    r.faskes_desc === "All" ? "NASIONAL" : r.faskes_desc
+                  )
+                )}
+                opts={{
+                  height: 900,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <TableMonitoringFaktorRisiko titleTable="Tabel Data Agregat" />
     </div>
   );
 }
